03d800200b2e0e746edd0b3aeec92ede476e477a
VSvetlakov <116279684+VSvetlakov@users.noreply.github.com>
2024-01-05 23:42:23 +0400

    v.4

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,6 +4,15 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return NumberUtils.toInt(str,0) > 0;
+        if (!NumberUtils.isCreatable(str))
+            return false;
+
+        if (NumberUtils.toInt(str) > 0)
+            return true;
+
+        if (NumberUtils.toDouble(str) > 0)
+            return true;
+
+        return false;
     }
 }
